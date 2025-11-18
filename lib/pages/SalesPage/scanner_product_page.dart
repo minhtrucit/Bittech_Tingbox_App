d88ff00e5fbac66880091b4eb04cb3aab3c319f3
Huy Tran Lam <tlhuy02@gmail.com>
2025-11-18 13:27:52 +0700

    feat: add package camera and config permission required

---
+++ lib/pages/SalesPage/scanner_product_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class ScannerProductPage extends StatefulWidget {
+  const ScannerProductPage({super.key});
+
+  @override
+  State<ScannerProductPage> createState() => _ScannerProductPageState();
+}
+
+class _ScannerProductPageState extends State<ScannerProductPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+
+  }
+}
