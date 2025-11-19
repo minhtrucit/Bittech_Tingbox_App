ae20dfa58da2914637719834fc698c7318380ca2
Huy Tran Lam <tlhuy02@gmail.com>
2025-11-19 09:16:22 +0700

    feat: update system overlay

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ting_box/pages/AuthPage/auth.dart';
 import 'package:ting_box/pages/base_page.dart';
@@ -7,6 +8,10 @@ import 'package:ting_box/services/auth_services.dart';
 import 'common/theme.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarBrightness: Brightness.dark,
+        statusBarIconBrightness: Brightness.dark
+  ));
   runApp(const MyApp());
 }
 
