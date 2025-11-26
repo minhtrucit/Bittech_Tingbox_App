03809a2a8aff264e36fc612325c5768ffea1122e
Huy Tran Lam <tlhuy02@gmail.com>
2025-11-26 21:09:29 +0700

    feat: update product skeleton list

--- lib/pages/ProductPage/ui/products_list_page.dart
+++ lib/pages/ProductPage/ui/products_list_page.dart
@@ -113,30 +113,31 @@ class _ProductsListPageState extends State<ProductsListPage>
                 children: [
                   _buildSearchBar(),
                   _buildFilterRow(),
-                  isLoading
-                      ? ProductsListSkeleton()
-                      : Expanded(
-                        child: BlocBuilder<ProductBloc, ProductState>(
-                          builder: (context, state) {
-                            // Show skeleton when loading or searching
-                            if (state is ProductLoading || isSearching) {
-                              return const ProductsListSkeleton();
-                            }
+                  Expanded(
+                    child:
+                        isLoading
+                            ? const ProductsListSkeleton()
+                            : BlocBuilder<ProductBloc, ProductState>(
+                              builder: (context, state) {
+                                // Show skeleton when loading or searching
+                                if (state is ProductLoading || isSearching) {
+                                  return const ProductsListSkeleton();
+                                }
 
-                            if (state is ProductLoadProductsSuccess) {
-                              final filteredProducts = _filterProducts(
-                                state.products,
-                              );
+                                if (state is ProductLoadProductsSuccess) {
+                                  final filteredProducts = _filterProducts(
+                                    state.products,
+                                  );
 
-                              return filteredProducts.isEmpty
-                                  ? _buildEmptyState()
-                                  : _buildProductGrid(filteredProducts);
-                            }
+                                  return filteredProducts.isEmpty
+                                      ? _buildEmptyState()
+                                      : _buildProductGrid(filteredProducts);
+                                }
 
-                            return _buildEmptyState();
-                          },
-                        ),
-                      ),
+                                return _buildEmptyState();
+                              },
+                            ),
+                  ),
                 ],
               ),
               _buildAddButton(context),
