cb0329983430c6bddb24109b0bb0aae9d4958f59
Huy Tran Lam <tlhuy02@gmail.com>
2025-11-29 23:26:27 +0700

    feat: handle case cash order

--- lib/pages/SalesPage/Components/confirm_order_dialog.dart
+++ lib/pages/SalesPage/Components/confirm_order_dialog.dart
@@ -104,6 +104,20 @@ class _ConfirmOrderDialogState extends State<ConfirmOrderDialog> {
             );
           } else {
             // Todo handle for cash method
+            DialogUtils.showAppDialog(
+              context: context,
+              title: 'Xác nhận đã thanh toán',
+              content: 'Hãy xác nhận đã thanh toán đầy đủ bằng tiền mặt',
+              onFirstAction: () {
+                Navigator.pop(context);
+              },
+              firstActionText: 'OK',
+              onSecondAction: () {
+                Navigator.pop(context);
+              },
+              secondActionText: 'Cancel',
+            );
+
           }
           
         }
